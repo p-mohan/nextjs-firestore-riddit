2b2c32c53c7c4370c495a9c27714491a5a6c6252
p-mohan <p-mohan@users.noreply.github.com>
2026-02-10 15:04:40 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { RecoilRoot } from "recoil";
-import { theme } from "../chakra/theme";
+import { system } from "../chakra/theme";
 import Layout from "@/components/Layout";
 export default function App({
   Component,
@@ -9,7 +9,7 @@ export default function App({
 }: AppProps) {
   return (
     <RecoilRoot>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider value={system}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
