ebbd13914b594106a9f9d676f2260b8b88302d4f
p-mohan <p-mohan@users.noreply.github.com>
2026-02-10 15:02:07 +0530

    Update button.recipe.ts

--- src/chakra/button.recipe.ts
+++ src/chakra/button.recipe.ts
@@ -1,1 +1,54 @@
 
+import { defineRecipe } from "@chakra-ui/react";
+
+export const buttonRecipe = defineRecipe({
+  base: {
+    borderRadius: "60px",
+    fontSize: "10pt",
+    fontWeight: "700",
+    _focus: {
+      boxShadow: "none",
+    },
+  },
+
+  variants: {
+    size: {
+      sm: {
+        fontSize: "8pt",
+      },
+      md: {
+        fontSize: "10pt",
+      },
+    },
+
+    variant: {
+      solid: {
+        color: "white",
+        bg: "blue.500",
+        _hover: {
+          bg: "blue.400",
+        },
+      },
+
+      outline: {
+        color: "blue.500",
+        border: "1px solid",
+        borderColor: "blue.500",
+      },
+
+      oauth: {
+        height: "34px",
+        border: "1px solid",
+        borderColor: "gray.300",
+        _hover: {
+          bg: "gray.50",
+        },
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: "md",
+    variant: "solid",
+  },
+});
